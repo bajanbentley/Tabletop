1bada8ff391162af22241286afd673f0e9f4afaa
bajanbentley <kylebentley@gmx.com>
2017-03-21 11:30:28 -0400

    Formatting - comments
    added comments and formatted to make easier to understand

--- scenes/tictactoe/js/scene.js
+++ scenes/tictactoe/js/scene.js
@@ -9,7 +9,36 @@ function init(){
 }
 init();
 
-//game object prototypes
+/************************************************************
+* adding gameobjects to the scene
+************************************************************/
+var x1 = xPiece();
+scene.add(x1);
+x1.position.set(-1,0,0);
+
+var o1 = oPiece();
+scene.add(o1);
+o1.position.set(1,0,0);
+
+
+/************************************************************
+* Main render function followed by animations
+************************************************************/
+function render() {
+	requestAnimationFrame( render );
+  animate();
+	renderer.render( scene, camera );
+}
+render();
+
+function animate(){
+  x1.rotation.y += 0.05;
+  o1.rotation.y += 0.05;
+}
+
+/************************************************************
+* GameObject constructors
+************************************************************/
 
 //returns a new x game object
 function xPiece(){
@@ -26,31 +55,10 @@ function xPiece(){
   return group;
 }
 
+//returns a new o game object
 function oPiece(){
   var geometry = new THREE.RingGeometry( 0.25, 0.5, 32 );
   var material = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
   var mesh = new THREE.Mesh( geometry, material );
   return mesh;
-}
-
-var x1 = xPiece();
-scene.add(x1);
-x1.position.set(-1,0,0);
-
-var o1 = oPiece();
-scene.add(o1);
-o1.position.set(1,0,0);
-
-
-//main render function
-function render() {
-	requestAnimationFrame( render );
-  animate();
-	renderer.render( scene, camera );
-}
-render();
-
-function animate(){
-  x1.rotation.y += 0.05;
-  o1.rotation.y += 0.05;
 }
