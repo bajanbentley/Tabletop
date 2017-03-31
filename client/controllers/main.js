454e78e33a6bcee0262be9e57a599b1fd9820141
4ndr3wch4n <chan.andrew1995@gmail.com>
2017-03-30 22:54:34 -0400

    test

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -173,7 +173,11 @@ app.controller('four', function($scope) {
           } );*/
       	});
 
+      geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    	material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
+    	mesh = new THREE.Mesh( geometry, material );
+    	scene.add( mesh );
 
     	renderer = new THREE.WebGLRenderer();
     	renderer.setSize( window.innerWidth, window.innerHeight );
