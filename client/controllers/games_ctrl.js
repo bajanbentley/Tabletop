aee521d3beade6c03042a9712d35f8cf2ef3d198
Andrew Chan <chan.andrew1995@gmail.com>
2017-04-10 15:37:13 -0400

    Update games_ctrl.js

--- client/controllers/games_ctrl.js
+++ client/controllers/games_ctrl.js
@@ -1,6 +1,15 @@
 var app = angular.module('views');
 
-app.controller('gamesController', function($scope, $location) {
+app.controller('gamesController', function($scope, $location, loginAuth) {
+  /************************************
+  *           Check login
+  **************************************/
+  $scope.navbarTemplate = './templates/navbar_nologged.htm';
+
+  var testLogged = (loginAuth.getLogged() || localStorage.getItem('id_token') != null) ? true : false ;
+  if(!testLogged) { // test for logged in
+    $location.path('/login');
+  }
   /*$scope.switchToWar = function() {
     $location.path("/war");
   }*/
